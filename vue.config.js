35f296fb18cf13fb9453619671fb49da3d4757b1
extretessestric <jigerjain4.gj@gmail.com>
2025-11-07 08:57:31 +0530

    changes

--- vue.config.js
+++ vue.config.js
@@ -1,10 +1,7 @@
 module.exports = {
-  publicPath: process.env.NODE_ENV === "production" ? "/portfolio/" : "/",
+  publicPath: "/portfolio/",
 
   chainWebpack: config => {
     config.module.rules.delete("eslint");
-  },
-
-  outputDir: "dist",
-  assetsDir: "",
+  }
 };
