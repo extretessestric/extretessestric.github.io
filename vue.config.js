42aed7800b944950be322402d827fac3dd0f4de0
extretessestric <jigerjain4.gj@gmail.com>
2025-11-07 10:05:28 +0530

    Merge branch 'main' of https://github.com/extretessestric/extretessestric.github.io

--- vue.config.js
+++ vue.config.js
@@ -1,7 +1,10 @@
+/** @type {import('@vue/cli-service').ProjectOptions} */
 module.exports = {
+  // Root deploy → no subpath
   publicPath: "/",
 
   chainWebpack: config => {
+    // keep your existing rule
     config.module.rules.delete("eslint");
   }
 };
